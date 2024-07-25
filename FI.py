0fa4ca7e4c8e44181eafb1b9eac525773b5987c2
Murilo Eziliano <77080184+MEziliano@users.noreply.github.com>
2024-07-25 18:44:23 -0300

    Update FI.py

--- FI.py
+++ FI.py
@@ -41,7 +41,7 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 try:
     PATH = r"C:\Users\U6094291\Desktop\StockPrice\customer_data_2.csv"if platform.system() == "Windows" else r"/home/usuario/Forex/customer_data_2.csv"
 except:
-    PATH = r".\customer_data_2.csv"
+    PATH = r"./customer_data_2.csv"
 @st.cache_data
 def load_data(path: str):
     # Function to load data
