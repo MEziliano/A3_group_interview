f8095100537a22eb4e0079d8b47e3602d14ef87a
Murilo Eziliano <77080184+MEziliano@users.noreply.github.com>
2024-07-25 18:48:18 -0300

    Update FI.py

--- FI.py
+++ FI.py
@@ -38,10 +38,10 @@ color_palettes = {
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-try:
-    PATH = r"C:\Users\U6094291\Desktop\StockPrice\customer_data_2.csv"if platform.system() == "Windows" else r"/home/usuario/Forex/customer_data_2.csv"
-except:
-    PATH = r"./customer_data_2.csv"
+
+#PATH = r"C:\Users\U6094291\Desktop\StockPrice\customer_data_2.csv"if platform.system() == "Windows" else r"/home/usuario/Forex/customer_data_2.csv"
+
+PATH = r"./customer_data_2.csv"
 @st.cache_data
 def load_data(path: str):
     # Function to load data
